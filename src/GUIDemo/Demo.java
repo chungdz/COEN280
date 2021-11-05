7a4d6d35d599ef625d7a8f94deabd702e0509794
Yunfan Hu <1033718596@qq.com>
2021-11-05 16:59:26 -0700

    add Jcombobox

--- src/GUIDemo/Demo.java
+++ src/GUIDemo/Demo.java
@@ -4,6 +4,8 @@
  */
 package GUIDemo;
 import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
 import java.util.*;
 import Table_Generated.Business;
 import java.awt.*;
@@ -12,9 +14,17 @@ import java.awt.*;
  * @author 10337
  */
 public class Demo extends JFrame {
-    
     ArrayList<JCheckBox> jb_list1;
     HashSet<String> category_selected;
+    String business_bool = "AND";
+    
+    private void print_list_1(){
+        String res = "";
+        for(String s: category_selected){
+            res += s + ", ";
+        }
+        System.out.println("category selected changes: " + res);
+    }
     
     private void add_button_first(JPanel jp){
         jb_list1 = new ArrayList<>();
@@ -26,7 +36,6 @@ public class Demo extends JFrame {
             jCheckBox_cur.setBackground(color);
             jCheckBox_cur.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
-//                    jCheckBoxl1ActionPerformed(evt);
                     AbstractButton abstractButton = (AbstractButton) evt.getSource();
                     String cur_text = abstractButton.getText();
                     boolean selected = abstractButton.getModel().isSelected();
@@ -36,6 +45,7 @@ public class Demo extends JFrame {
                     else{
                         category_selected.remove(cur_text);
                     }
+                    print_list_1();
                 }
             });
             jb_list1.add(jCheckBox_cur);
@@ -50,24 +60,38 @@ public class Demo extends JFrame {
         jp1.setVisible(true);
         jp1.setLayout(new GridLayout(0, 2));
         jp1.setBackground(new java.awt.Color(204, 204, 255));
+        jp1.setBorder(new TitledBorder(new EtchedBorder(), "Categories"));
         add_button_first(jp1);
         return jp1;
+    }
+    
+    private JComboBox<String> firstJcb(){
+        String labels[] = {"AND", "OR"};
+        JComboBox<String> comboBox = new JComboBox<String>(labels);
+        comboBox.setEditable(true);
+        comboBox.setBounds(0, 421, 350, 25);
+        comboBox.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                JComboBox<String> cb = (JComboBox<String>) e.getSource();
+                business_bool = cb.getSelectedItem().toString();
+                System.out.println("Business select state changes to " + business_bool);
+            }
+        });
+        return comboBox;
     }
     
     public Demo() {
         Container container = this.getContentPane();
         // 设置流布局管理器，2是右对齐，后两个参数分别为组件间的水平间隔和垂直间隔
         this.setLayout(null);
-        JPanel jp1 = firstjp();
-        
 //        JScrollPane scrollPane = new JScrollPane(jp1);
 //        scrollPane.setBounds(0, 0, 350, 420);
 //        jp1.setPreferredSize(new Dimension(350, 420));
 //        jp1.revalidate();
-        container.add(jp1);
-        
+        container.add(firstjp());
+        container.add(firstJcb());
 
-        this.setSize(1200, 900);
+        this.setSize(2000, 1000);
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
